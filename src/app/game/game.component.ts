ccc1b3c566be95b369ea1cf02b309dd0191c7db9
Simon Vitt <ttivnomis@gmail.com>
2023-01-04 13:04:58 +0100

    first playable version

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 @Component({
   selector: 'app-game',
@@ -7,7 +11,39 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
-  takeCard(){
-    this.pickCardAnimation = true;
+  currentCard: string = '';
+  game: Game;
+
+  constructor(public dialog: MatDialog) {
+    this.game = new Game();
+  }
+
+  /*ngOnInit(){
+    this.newGame();
+  }*/
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop()!;
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+
+        this.game.currentPlayer++;
+        this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+      }, 1200);
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if(name && name.length > 0 ){
+        this.game.players.push(name);
+      }
+    });
   }
 }
